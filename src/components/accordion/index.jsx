9add37e038b04584a027b9281254fb34aa5b0869
nuhptr <nugrahaadi733@gmail.com>
2024-04-18 10:12:28 +0700

    components: accordion done

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -3,24 +3,65 @@
 
 import { useState } from "react"
 
+import "./styles.css"
 import { data } from "./data"
 
 export default function Accordion() {
-   const [selected, setSelected] = useState()
+   const [selected, setSelected] = useState(null)
+   const [multipleSelected, setMultipleSelected] = useState([])
 
-   const handleSingleSelect = (getCurrentId) => {}
+   const [enableMultiSelect, setEnableMultiSelect] = useState(false)
+
+   const handleSingleSelect = (getCurrentId) => {
+      const selectedId = selected === getCurrentId ? null : getCurrentId
+
+      setSelected(selectedId)
+   }
+
+   const handleMultipleSelect = (getCurrentId) => {
+      let copyMultipleSelected = [...multipleSelected]
+      const findIndexCurrentId = copyMultipleSelected.indexOf(getCurrentId)
+
+      if (findIndexCurrentId === -1) {
+         copyMultipleSelected.push(getCurrentId)
+      } else {
+         copyMultipleSelected.splice(findIndexCurrentId, 1)
+      }
+
+      setMultipleSelected(copyMultipleSelected)
+   }
 
    return (
       <div className="wrapper">
+         <button className="btn-multiselect" onClick={() => setEnableMultiSelect((prev) => !prev)}>
+            {!enableMultiSelect ? "Enabled" : "Disabled"} multi select
+         </button>
+
          <div className="accordion">
-            {data &&
-               data.length > 0 &&
+            {data.length > 0 &&
                data.map((dataItem) => (
-                  <div className="item" key={dataItem.id}>
-                     <div onClick={() => handleSingleSelect(dataItem.id)} className="title">
-                        <h3>{dataItem.question}</h3>
-                        <span>+</span>
+                  <div className="accordion-item" key={dataItem.id}>
+                     <div
+                        onClick={
+                           enableMultiSelect
+                              ? () => handleMultipleSelect(dataItem.id)
+                              : () => handleSingleSelect(dataItem.id)
+                        }
+                        className="accordion-title">
+                        <h3 className="accordion-title__question">{dataItem.question}</h3>
+                        <span>
+                           {selected === dataItem.id || multipleSelected.indexOf(dataItem.id) !== -1
+                              ? "-"
+                              : "+"}
+                        </span>
                      </div>
+                     {enableMultiSelect
+                        ? multipleSelected.indexOf(dataItem.id) !== -1 && (
+                             <div className="accordion-content">{dataItem.answer}</div>
+                          )
+                        : selected === dataItem.id && (
+                             <div className="accordion-content">{dataItem.answer}</div>
+                          )}
                   </div>
                ))}
 
