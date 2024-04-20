f84673604291f9c5b187b0fde44ee2cbffb14d14
nuhptr <nugrahaadi733@gmail.com>
2024-04-20 12:53:17 +0700

    components: entry point update

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import { ImageSlider } from "./slider"
 import { Accordion } from "./accordion"
 import { ColorGenerator } from "./color-generator"
 import { StarRating } from "./star-rating"
+import { InfiniteLoad } from "./infinite-load"
 
-export { ImageSlider, Accordion, ColorGenerator, StarRating }
+export { ImageSlider, Accordion, ColorGenerator, StarRating, InfiniteLoad }
