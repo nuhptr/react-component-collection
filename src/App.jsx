f10b070f65374276825bce9656fb21616600bbe2
nuhptr <nugrahaadi733@gmail.com>
2024-04-20 12:48:41 +0700

    app: add infinite load

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Accordion, ColorGenerator, ImageSlider, StarRating } from "./components"
+import { Accordion, ColorGenerator, ImageSlider, InfiniteLoad, StarRating } from "./components"
 
 export default function App() {
    return (
@@ -10,7 +10,8 @@ export default function App() {
          {/* Star Rating */}
          <StarRating numberStarts={5} />
          {/* Slider */}
-         <ImageSlider />
+         <ImageSlider page="1" limit="10" url="https://picsum.photos/v2/list" />
+         <InfiniteLoad />
       </main>
    )
 }
