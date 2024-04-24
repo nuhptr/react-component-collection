aea808973fe75e93260fc7d84295c602c523cb2e
nuhptr <nugrahaadi733@gmail.com>
2024-04-24 07:36:13 +0700

    delete: change to next

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import { ImageSlider } from "./slider"
-import { Accordion } from "./accordion"
-import { ColorGenerator } from "./color-generator"
-import { StarRating } from "./star-rating"
-import { InfiniteLoad } from "./infinite-load"
-
-export { ImageSlider, Accordion, ColorGenerator, StarRating, InfiniteLoad }
