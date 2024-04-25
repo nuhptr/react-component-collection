81b9737f6fa80380564f8a0fd5f958df4bf78bd2
nuhptr <nugrahaadi733@gmail.com>
2024-04-25 19:36:34 +0700

    app: layout add themeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 
 import "./globals.css"
+import DarkModeThemeProvider from "@/app/dark-mode/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -15,7 +16,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
    return (
       <html lang="en">
          <body className={inter.className} suppressHydrationWarning={true}>
-            {children}
+            <DarkModeThemeProvider>{children}</DarkModeThemeProvider>
          </body>
       </html>
    )
